822562c73d808d26c289d1cde5fbef682d4ad5ab
cepharma <duarteadrian3710@gmail.com>
2022-05-20 16:52:03 -0400

    primer commit

---
+++ useFetch.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+
+export const useFetch = (url) => {
+
+    // const isMounted = useRef(true);
+    const [state, setState] = useState({ data: null, loading: true, error: null });
+    
+    // useEffect(() => {
+    //     return (() => {
+    //         isMounted.current = false;
+    //     });
+    // });
+
+    useEffect(() => {
+
+        setState({ data: null, loading: true, error: null });
+
+        fetch(url)
+            .then(resp => resp.json())
+            .then(data => {
+
+                    setState({
+                        loading: false,
+                        error: null,
+                        data
+                    });
+
+            });
+
+    }, [url]);
+
+    return state;
+
+}
